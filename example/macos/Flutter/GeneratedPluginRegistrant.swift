445f8d6e17e816c49aced7152e6099bfa7660a73
ishamimul <skshamimul@cayto.jp>
2023-05-26 10:33:23 +0600

    flutter clean

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import image_eadge_detection
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ImageEadgeDetectionPlugin.register(with: registry.registrar(forPlugin: "ImageEadgeDetectionPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
